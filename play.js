08acbb10d6aff6db91df5f5b6cf0395113a15db5
Roylu7384 <roylu7384@gmail.com>
2022-02-16 15:04:28 -0500

    setup user input interface and exit command

--- play.js
+++ play.js
@@ -3,3 +3,22 @@ const {connect} = require('./client');
 
 console.log("Connecting ...");
 connect();
+
+// setup interface to handle user input from stdin
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
+};
+const handleUserInput = function(data) {
+  if (data === '\u0003') {
+    console.log('Thanks for playing snake, ciao!');
+    process.exit();
+  }
+};
+
+const stdin = setupInput();
+stdin.on('data', handleUserInput);
