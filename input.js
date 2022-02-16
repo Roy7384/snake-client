c4672b5e9ff69bddf9b3a9042b9e7e996ee060b0
Roylu7384 <roylu7384@gmail.com>
2022-02-16 15:35:50 -0500

    add wasd control for snake movement in input.js

--- input.js
+++ input.js
@@ -1,6 +1,11 @@
+let connection;
+
 // setup interface to handle user input from stdin
 
-const setupInput = function() {
+const setupInput = function(conn) {
+
+  connection = conn;
+
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -11,9 +16,16 @@ const setupInput = function() {
       console.log('Thanks for playing snake, ciao!');
       process.exit();
     }
+    const keys = ['w', 'a', 's', 'd'];
+    const move = ['Move: up', 'Move: left', 'Move: down', 'Move: right'];
+    if (keys.includes(data)) {
+      const input = move[keys.indexOf(data)];
+      connection.write(input);
+    }
   };
-
+  
   stdin.on('data', handleUserInput);
+
   return stdin;
 };
 
