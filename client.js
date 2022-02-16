145c31e8821562b1c7ce18ebd1cde20244226e68
Roylu7384 <roylu7384@gmail.com>
2022-02-16 14:49:12 -0500

    write data to server when connection is made'

--- client.js
+++ client.js
@@ -5,7 +5,10 @@ const connect = function() {
     host: '165.227.47.243' , // IP address here,
     port: '50541' // PORT number here,
   });
-
+  conn.on('connect', () => {
+    console.log('Connection made');
+    conn.write('Name: ROY');
+  });
   // interpret incoming data as text
   conn.setEncoding("utf8");
   conn.on('data', (data) => {
