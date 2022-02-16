79cbbc2b9cbd6e23b8c7fc5e682eb29c92d0638a
Roylu7384 <roylu7384@gmail.com>
2022-02-16 15:11:40 -0500

    refactor input.js to have exit command inside input interface setup function

--- play.js
+++ play.js
@@ -1,24 +1,8 @@
 const {connect} = require('./client');
-
+const {setupInput} = require('./input');
 
 console.log("Connecting ...");
 connect();
 
-// setup interface to handle user input from stdin
+setupInput();
 
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  return stdin;
-};
-const handleUserInput = function(data) {
-  if (data === '\u0003') {
-    console.log('Thanks for playing snake, ciao!');
-    process.exit();
-  }
-};
-
-const stdin = setupInput();
-stdin.on('data', handleUserInput);
